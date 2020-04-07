5f5fd5123085c47e523b973b4fbfeab5624896cc
shishishi1320 <shishishi.1320@gmail.com>
2020-04-07 19:37:09 +0900

    chatspeac

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,15 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
@@ -15,5 +24,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email)
+  end
+  def index
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 end
